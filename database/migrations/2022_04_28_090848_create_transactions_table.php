90cd7a3188e43136d6d38cb846600446998687ad
alafgani1000 <cppol08@gmail.com>
2022-04-28 16:17:15 +0700

    membuat table saw

---
+++ database/migrations/2022_04_28_090848_create_transactions_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->integer('alternative_id');
+            $table->integer('attribute_id');
+            $table->decimal('value', $precision = 19, $scale = 4);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
